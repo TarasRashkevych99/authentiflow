469f866bb247ae80a506119fd427de84eb94e5ab
lucavianunitn <luca.vian@studenti.unitn.it>
2023-12-06 14:48:55 +0100

    Added conceptual role assignment in two-party communication

--- src/app.js
+++ src/app.js
@@ -34,8 +34,69 @@ const io = require('socket.io')(app, {
 
 const port = 3000 || process.env.PORT;
 
+let users_connected = [];
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    n_users = io.engine.clientsCount;
+    id_last_client = socket.id;
+    users_connected.push(id_last_client);
+
+    switch (n_users) {
+        case 1:
+            //io.to(id_last_client).emit
+            console.log(
+                id_last_client +
+                    ': You are the first and only client, you need to generate a key for a currently unknown one'
+            );
+            break;
+        case 2:
+            console.log(
+                id_last_client +
+                    ': You are the second client, you need to receive a key from ' +
+                    users_connected[0]
+            );
+            break;
+        default:
+            console.log(
+                id_last_client + ': Sorry, there are already two users'
+            );
+            break;
+    }
+
+    socket.on('disconnect', () => {
+        id_disconnected_client = socket.id;
+
+        var index = users_connected.indexOf(id_disconnected_client);
+
+        if (index !== -1) {
+            users_connected.splice(index, 1);
+
+            if (index > 1) {
+                //only the first 2 user in the array are involved
+                console.log(
+                    'A client not involved in the chat just disconnected'
+                );
+            } else {
+                if (users_connected.length > 1) {
+                    //so, it's still possible establish immediately a communiction
+                    console.log(
+                        users_connected[0] +
+                            ': must to generate a key to use with ' +
+                            users_connected[1]
+                    );
+                    console.log(
+                        users_connected[1] +
+                            ': must to wait a key from ' +
+                            users_connected[0]
+                    );
+                } else {
+                    console.log(
+                        users_connected[0] +
+                            ': You are the first and only client, you need to generate a key for a currently unknown one'
+                    );
+                }
+            }
+        }
+    });
 
     socket.on('message', (message) => {
         console.log(message);
