6e98fe20c45b05154818c56c07bee5e05b2f51fd
lucavianunitn <luca.vian@studenti.unitn.it>
2023-12-12 15:58:39 +0100

    add logic to join and leave a room

--- src/authentiflow-webapp/src/components/AuthentiflowChat.vue
+++ src/authentiflow-webapp/src/components/AuthentiflowChat.vue
@@ -1,25 +1,36 @@
 <template>
-  <!-- <div id="app">
-    <h1>Authentiflow</h1>
-    <input type="text" ref="roomId" />
-    <button @click="handleJoin">Join Room</button>
-  </div> -->
   <v-sheet width="300" class="mx-auto">
-    <v-form fast-fail @submit.prevent>
+    <h1>Authentiflow</h1>
+    <br />
+    <div :hidden="!findRoomDiv">
       <v-text-field
-        v-model="firstName"
-        label="First name"
-        :rules="firstNameRules"
+        :disabled="!roomTextfield"
+        label="Room Phrase"
+        :rules="RoomPhraseRules"
+        ref="roomPhrase"
       ></v-text-field>
 
-      <v-text-field
-        v-model="lastName"
-        label="Last name"
-        :rules="lastNameRules"
-      ></v-text-field>
+      <v-btn
+        :loading="loading"
+        :disabled="!joinButton"
+        class="flex-grow-1"
+        height="48"
+        variant="tonal"
+        @click="handleJoin"
+        block
+      >
+        Join Room
+      </v-btn>
 
-      <v-btn type="submit" block class="mt-2">Submit</v-btn>
-    </v-form>
+      <v-btn :hidden="!cancelButton" height="48" variant="plain" @click="cancelJoin" block>
+        Search for another room
+      </v-btn>
+    </div>
+    <div :hidden="findRoomDiv">
+      <h2>Here will appear the chat</h2>
+
+      <v-btn height="48" variant="plain" @click="leaveRoom" block> Leave the room </v-btn>
+    </div>
   </v-sheet>
 </template>
 
@@ -27,19 +38,66 @@
 import io from 'socket.io-client'
 
 export default {
-  data() {},
+  data: () => ({
+    loading: false,
+    cancelButton: false,
+    joinButton: false,
+    roomTextfield: true,
+    findRoomDiv: true
+  }),
+  computed: {
+    RoomPhraseRules() {
+      return [
+        (value) => {
+          if (value?.length > 10) {
+            this.joinButton = true
+            return true
+          }
+          this.joinButton = false
+          return 'The Room Phrase must be at least 10 characters.'
+        }
+      ]
+    }
+  },
+  watch: {
+    loading(val) {
+      if (!val) return
+
+      setTimeout(() => (this.cancelButton = true), 3000)
+    }
+  },
   mounted() {
     this.socket = io('wss://localhost:3000')
+    this.socket.on('confirmJoin', (data) => {
+      this.findRoomDiv = false
+    })
   },
   methods: {
     handleJoin() {
-      const roomId = this.$refs.roomId.value
-      console.log(roomId)
-      this.socket.emit('joinMsg', roomId)
+      const roomPhrase = this.$refs.roomPhrase.value
+      console.log(roomPhrase)
+      this.socket.emit('joinMsg', roomPhrase)
+      this.roomTextfield = false
+      this.loading = true
+    },
+    cancelJoin() {
+      const roomPhrase = this.$refs.roomPhrase.value
+      this.socket.emit('cancelJoinMsg', roomPhrase)
+      this.roomTextfield = true
+      this.loading = false
+      this.cancelButton = false
+    },
+    leaveRoom() {
+      this.findRoomDiv = true
+      this.roomTextfield = true
+      this.loading = false
+      this.cancelButton = false
     }
   },
   beforeUnmount() {
+    //to fix
     if (this.socket) {
+      this.socket.emit('cancelJoinMsg', roomPhrase)
       this.socket.disconnect()
     }
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const io = require('socket.io');
+const crypto = require('crypto');
 
 const clientAuthMiddleware = () => (req, res, next) => {
     if (!req.client.authorized) {
@@ -36,11 +37,44 @@ const webSocket = io(app);
 
 const port = 3000 || process.env.PORT;
 
+var roomPhraseToUser = {};
 webSocket.on('connection', (socket) => {
-    console.log('Connected');
+    console.log(`${socket.id} connected \n`);
 
-    socket.on('joinMsg', (roomId) => {
-        console.log('A user wants to join in room ' + roomId);
+    socket.on('joinMsg', (roomPhrase) => {
+        if (roomPhrase.length <= 10) return;
+
+        const roomHash = crypto.createHash('sha256');
+        roomHash.update(roomPhrase);
+        const roomDigest = roomHash.digest('hex');
+        console.log(
+            `${socket.id} wants to join in room with phrase ${roomPhrase} (digest: ${roomDigest}) \n`
+        );
+
+        if (!roomPhraseToUser[roomDigest]) {
+            roomPhraseToUser[roomDigest] = socket.id;
+        } else {
+            console.log(
+                `${socket.id} and ${roomPhraseToUser[roomDigest]} can join in room with phrase ${roomPhrase} (digest: ${roomDigest}) \n`
+            );
+            webSocket
+                .to(socket.id)
+                .to(roomPhraseToUser[roomDigest])
+                .emit('confirmJoin', true);
+
+            delete roomPhraseToUser[roomDigest];
+        }
+    });
+
+    socket.on('cancelJoinMsg', (roomPhrase) => {
+        const roomHash = crypto.createHash('sha256');
+        roomHash.update(roomPhrase);
+        const roomDigest = roomHash.digest('hex');
+
+        if (roomPhraseToUser[roomDigest] == socket.id) {
+            delete roomPhraseToUser[roomDigest];
+            console.log(`Deleted room proposed by ${socket.id} \n`);
+        }
     });
 });
 
